2915daa49327f0c84ab94c0bb976cf9b7f5de480
NohaMohamed <nohamohamedali1993@gmail.com>
2019-02-16 00:30:17 +0200

    setup architecture

---
+++ Assessment-Task/Movies/MoviesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MoviesListViewController.swift
+//  Assessment-Task
+//
+//  Created by Noha  on 2/15/19.
+//  Copyright © 2019 Noha . All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
